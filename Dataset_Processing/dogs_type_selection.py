dd126a992ae97241dcb8e2bb36376237abef9fee
arieloren <78952112+arieloren@users.noreply.github.com>
2021-03-22 11:15:41 +0200

    Update dogs_type_selection.py

--- Dataset_Processing/dogs_type_selection.py
+++ Dataset_Processing/dogs_type_selection.py
@@ -1,21 +1,26 @@
 # Libraries
 import re
-import os
 import shutil
 
 
-def dog_type_selection(orig_path, dest_path, dogs_names):  # dogs_name must be a list with strings part inside it
-    for dog in dogs_names:
+def dogs_type_selection(orig_path, dest_path, dogs_names):  # dogs_name must be a list with strings parts inside it
 
-        for subdir, dirs, files in os.walk(orig_path):
-            for filename in files:
-                filepath = subdir + os.sep + filename
-                if (re.findall(dog, filename)):  # check if the dogs name match the filename
-                    shutil.move(filepath, dest_path)
+    if len(dogs_names) != 0:
+
+        for dog in dogs_names:
+
+            for subdir, dirs, files in os.walk(orig_path):
+                for filename in files:
+                    filepath = subdir + os.sep + filename
+                    if (re.findall(dog, filename)):  # check if the dogs name match the filename
+                        shutil.move(filepath, dest_path)
+
+    else:
+        print("the dogs list is empty")
 
 
 if __name__ == '__main__':
     orig_path = 'C:/Users/user/Documents/Ariel/DL_lab_project/oxford_dog_dataset/images'
     dest_path = 'C:/Users/user/Documents/Ariel/DL_lab_project/oxford_dog_dataset/unwanted_dogs'
-    unwanted_list = ['Abyssinian', 'basset_hound']
-    dog_type_selection(orig_path, dest_path, unwanted_list)
+    unwanted_list = []
+    dogs_type_selection(orig_path, dest_path, unwanted_list)
